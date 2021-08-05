e9fc03584fb517a78e116d789fb059e391f50362
NasarKamish <Nasarkamish@gmail.com>
2021-08-05 09:44:05 +0200

    Updated file

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import datetime
 from flask import Flask, request, jsonify
 from flask_jwt import JWT, jwt_required, current_identity
 from flask_cors import CORS
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 db = "Shop.db"
 
@@ -24,7 +27,7 @@ def fetch_users():
         new_data = []
 
         for data in users:
-            new_data.append(User(data[0], data[3], data[4]))
+            new_data.append(User(data[0], data[4], data[5]))
     return new_data
 
 
@@ -35,21 +38,23 @@ def init_user_table():
     conn.execute("CREATE TABLE IF NOT EXISTS user(user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
                  "first_name TEXT NOT NULL,"
                  "last_name TEXT NOT NULL,"
+                 "Email TEXT NOT NULL,"
                  "username TEXT NOT NULL,"
-                 "password TEXT NOT NULL)")
+                 "password TEXT NOT NULL"
+                 ")")
     print("user table created successfully")
     conn.close()
 
 
-def init_history_table():
-    with sqlite3.connect(db) as conn:
-        conn.execute("CREATE TABLE IF NOT EXISTS History (history_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                     "day TEXT NOT NULL,"
-                     "total TEXT NOT NULL,"
-                     "date_created TEXT NOT NULL,"
-                     "user_id INTEGER NOT NULL,"
-                     "FOREIGN KEY (user_id) REFERENCES user (user_id))")
-    print("History table created successfully.")
+# def init_history_table():
+#     with sqlite3.connect(db) as conn:
+#         conn.execute("CREATE TABLE IF NOT EXISTS History (history_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+#                      "day TEXT NOT NULL,"
+#                      "total TEXT NOT NULL,"
+#                      "date_created TEXT NOT NULL,"
+#                      "user_id INTEGER NOT NULL,"
+#                      "FOREIGN KEY (user_id) REFERENCES user (user_id))")
+#     print("History table created successfully.")
 
 
 def init_product_table():
@@ -57,27 +62,28 @@ def init_product_table():
         conn.execute("CREATE TABLE IF NOT EXISTS Product (product_id INTEGER PRIMARY KEY AUTOINCREMENT,"
                      "name TEXT NOT NULL,"
                      "price TEXT NOT NULL"
+                     "date TEXT NOT NULL"
                      ")")
     print("Product table created successfully.")
 
 
-def init_items_table():
-    with sqlite3.connect(db) as conn:
-        conn.execute("CREATE TABLE IF NOT EXISTS Items (items_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                     "quantity TEXT NOT NULL,"
-                     "date_created TEXT NOT NULL,"
-                     "history_id INTEGER NOT NULL,"
-                     "product_id INTEGER NOT NULL,"
-                     "FOREIGN KEY (history_id) REFERENCES History (history_id),"
-                     "FOREIGN KEY (product_id) REFERENCES Product (product_id))"
-                     )
-    print("Item table created successfully.")
+# def init_items_table():
+#     with sqlite3.connect(db) as conn:
+#         conn.execute("CREATE TABLE IF NOT EXISTS Items (items_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+#                      "quantity TEXT NOT NULL,"
+#                      "date_created TEXT NOT NULL,"
+#                      "history_id INTEGER NOT NULL,"
+#                      "product_id INTEGER NOT NULL,"
+#                      "FOREIGN KEY (history_id) REFERENCES History (history_id),"
+#                      "FOREIGN KEY (product_id) REFERENCES Product (product_id))"
+#                      )
+#     print("Item table created successfully.")
 
 
 init_user_table()
-init_history_table()
+# init_history_table()
 init_product_table()
-init_items_table()
+# init_items_table()
 users = fetch_users()
 
 username_table = {u.username: u for u in users}
@@ -117,19 +123,56 @@ def user_registration():
 
         first_name = request.form['first_name']
         last_name = request.form['last_name']
+        email = request.form['Email']
         username = request.form['username']
         password = request.form['password']
 
-        with sqlite3.connect(db) as conn:
-            cursor = conn.cursor()
-            cursor.execute("INSERT INTO user("
-                           "first_name,"
-                           "last_name,"
-                           "username,"
-                           "password) VALUES(?, ?, ?, ?)", (first_name, last_name, username, password))
-            conn.commit()
-            response["message"] = "success"
-            response["status_code"] = 201
+        # entered info is correct
+        info_val = False
+        if first_name != '' and last_name != '' and username != '' and password != '':
+            info_val = True
+        else:
+            info_val = False
+
+        # email validation
+        email_val = False
+
+        if email != "":
+            try:
+                sender_email_id = 'jimmy.local.shop.project@gmail.com'
+                receiver_email_id = email
+                password = "smsSHOP31314"
+                subject = "Local Shop Register"
+                msg = MIMEMultipart()
+                msg['From'] = sender_email_id
+                msg['To'] = receiver_email_id
+                msg['Subject'] = subject
+                body = "You're account has been verified"
+                msg.attach(MIMEText(body, 'plain'))
+                text = msg.as_string()
+                s = smtplib.SMTP('smtp.gmail.com', 587)
+                s.starttls()
+                s.login(sender_email_id, password)
+                s.sendmail(sender_email_id, receiver_email_id, text)
+                s.quit()
+                email_val = True
+            except:
+                email_val = False
+        else:
+            email_val = False
+
+        if email_val and info_val:
+            with sqlite3.connect(db) as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO user("
+                               "first_name,"
+                               "last_name,"
+                               "email,"
+                               "username,"
+                               "password) VALUES(?, ?, ?, ?, ?)", (first_name, last_name, email, username, password))
+                conn.commit()
+                response["message"] = "success"
+                response["status_code"] = 201
     return response
 
 
@@ -141,14 +184,15 @@ def create_product():
     if request.method == "POST":
         product_name = request.form['product_name']
         product_price = request.form['product_price']
-        # date_created = datetime.datetime.now()
+        date_created = datetime.datetime.now()
 
         with sqlite3.connect(db) as conn:
             cursor = conn.cursor()
             cursor.execute("INSERT INTO Product ("
                            "name,"
                            "price,"
-                           ") VALUES(?, ?)", (product_name, product_price))
+                           "date,"
+                           ") VALUES(?, ?)", (product_name, product_price, date_created))
             conn.commit()
             response["status_code"] = 201
             response['description'] = "Product added successfully"
@@ -206,6 +250,16 @@ def edit_post(product_id):
                 with sqlite3.connect(db) as conn:
                     cursor = conn.cursor()
                     cursor.execute("UPDATE product SET price =? WHERE id=?", (put_data["price"], product_id))
+                    conn.commit()
+
+                    response["content"] = "Content updated successfully"
+                    response["status_code"] = 200
+            if incoming_data.get("date") is not None:
+                put_data['date'] = incoming_data.get('date')
+
+                with sqlite3.connect(db) as conn:
+                    cursor = conn.cursor()
+                    cursor.execute("UPDATE product SET date =? WHERE id=?", (put_data["date"], product_id))
                     conn.commit()
 
                     response["content"] = "Content updated successfully"
